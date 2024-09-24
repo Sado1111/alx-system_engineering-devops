ef72909f3c5bce46e4e567585549fad06ca0b5b1
Sado1111 <sadohannahfabohns@gmail.com>
2024-09-24 07:03:09 +0100

    placed a character within regex

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -3,4 +3,4 @@
 # create a Ruby script that accepts one argument and pass it to a regular expression matching method
 # The regular expression must match hb{tt-ttttt}n
 
-puts ARGV[0].scan(/hbt{2-5}/).join
+puts ARGV[0].scan(/hbt{2-5}n/).join
