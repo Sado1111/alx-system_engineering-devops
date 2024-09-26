72d2cd30825e08963e77bcd0d6b10ce259c80aee
Sado1111 <sadohannahfabohns@gmail.com>
2024-09-26 19:25:14 +0100

    used the right shebang

--- 0x06-regular_expressions/5-beginning_and_end.rb
+++ 0x06-regular_expressions/5-beginning_and_end.rb
@@ -1,4 +1,4 @@
-#!/usr/bin/ruby
+#!/usr/bin/env ruby
 # create a Ruby script that accepts one argument and pass it to a regular expression matching method
 # The regex must matches a string that starts with h ends with n and can have any single character in between
 # Introducing the use of . in REGEX
